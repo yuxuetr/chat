5f364dcdede21e7dc94b7686b70d12d3235c76c9
yuxuetr <116985868+54yeying@users.noreply.github.com>
2024-05-31 01:27:01 +0800

    feat: refeactor chat_server and create notify_server

--- chat_server/src/main.rs
+++ chat_server/src/main.rs
@@ -1,16 +1,11 @@
 use anyhow::Result;
+use chat_server::{get_router, AppConfig};
 use tokio::net::TcpListener;
-use chat::{AppConfig, get_router};
 use tracing::{info, level_filters::LevelFilter};
-use tracing_subscriber::{
-  fmt::Layer,
-  layer::SubscriberExt,
-  util::SubscriberInitExt,
-  Layer as _
-};
+use tracing_subscriber::{fmt::Layer, layer::SubscriberExt, util::SubscriberInitExt, Layer as _};
 
 #[tokio::main]
-async fn main() -> Result<()>{
+async fn main() -> Result<()> {
   let layer = Layer::new().with_filter(LevelFilter::INFO);
   tracing_subscriber::registry().with(layer).init();
 
