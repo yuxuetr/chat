d43ee879dbbc1bb943d8abe1019cc95a9f96844d
yuxuetr <116985868+54yeying@users.noreply.github.com>
2024-06-18 14:25:10 +0800

    feat: support middlerware for request_id, server_time, verify_token

--- chat_server/src/handlers/chat.rs
+++ chat_server/src/handlers/chat.rs
@@ -1,7 +1,18 @@
-use axum::response::IntoResponse;
+use crate::User;
+use axum::{response::IntoResponse, Extension};
+use tracing::info;
+
+pub(crate) async fn create_chat_handler(Extension(user): Extension<User>) -> impl IntoResponse {
+  info!("user: {:?}", user);
+  "create chat"
+}
+pub(crate) async fn list_chat_handler(Extension(user): Extension<User>) -> impl IntoResponse {
+  info!("user: {:?}", user);
+  "list chat"
+}
 
 pub(crate) async fn update_chat_handler() -> impl IntoResponse {
-  "create chat"
+  "update chat"
 }
 
 pub(crate) async fn delete_chat_handler() -> impl IntoResponse {
--- chat_server/src/lib.rs
+++ chat_server/src/lib.rs
@@ -1,17 +1,20 @@
 mod config;
 mod error;
 mod handlers;
+mod middlewares;
 mod models;
 mod utils;
 
 use anyhow::Context;
 use axum::{
+  middleware::from_fn_with_state,
   routing::{get, patch, post},
   Router,
 };
 pub use config::AppConfig;
 use error::AppError;
 use handlers::*;
+use middlewares::{set_layer, verify_token};
 use models::User;
 use sqlx::PgPool;
 use std::{fmt, ops::Deref, sync::Arc};
@@ -33,21 +36,24 @@ pub(crate) struct AppStateInner {
 pub async fn get_router(config: AppConfig) -> Result<Router, AppError> {
   let state = AppState::try_new(config).await?;
   let api = Router::new()
-    .route("/signin", post(signin_handler))
-    .route("/signup", post(signup_handler))
+    .route("/chat", get(list_chat_handler).post(create_chat_handler))
     .route(
       "/chat/:id",
       patch(update_chat_handler)
         .delete(delete_chat_handler)
         .post(send_message_handler),
     )
-    .route("/chat/:id/message", get(list_message_handler));
+    .route("/chat/:id/message", get(list_message_handler))
+    .layer(from_fn_with_state(state.clone(), verify_token))
+    .route("/signin", post(signin_handler))
+    .route("/signup", post(signup_handler));
 
   let app = Router::new()
     .route("/", get(index_handler))
     .nest("/api", api)
     .with_state(state);
-  Ok(app)
+
+  Ok(set_layer(app))
 }
 
 // state.config => state.inner.config
